2572f4ecd898d28888dc1e3c57f1a5854fb824e5
arfeenadamji <arfeenm9@gmail.com>
2020-12-13 11:33:25 +0500

    First Commit

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
-export default function App() {
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex:1}}>
+    <Header headerText={'Albums'}/>
+    <AlbumList />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+AppRegistry.registerComponent('xdalbum', () => App)
+
